e56bff49936c08ef434d5d8214309d861686c082
Tom Segbers <tomsegbers@googlemail.com>
2020-01-22 14:24:56 +0100

    Add GoogleMap and Cleanup file structure

---
+++ src/components/simplemap.js
@@ -0,0 +1,49 @@
+//SRC: https://github.com/google-map-react/google-map-react
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => (
+    <div style={{
+        color: 'white',
+        background: 'grey',
+        padding: '15px 10px',
+        display: 'inline-flex',
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: '100%',
+        transform: 'translate(-50%, -50%)'
+    }}>
+        {text}
+    </div>
+);
+class SimpleMap extends Component {
+    static defaultProps = {
+        center: {
+            lat: 59.95,
+            lng: 30.33
+        },
+        zoom: 11
+    };
+
+    render() {
+        return (
+            // Important! Always set the container height explicitly
+            <div style={{ height: '100vh', width: '100%' }}>
+                <GoogleMapReact
+                    //bootstrapURLKeys={{ key: 'AIzaSyAlgLsmU20yV30l6XzFZHbulVA1ONCduiA' }}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                >
+                    <AnyReactComponent
+                        lat={59.955413}
+                        lng={30.337844}
+                        text="My Marker"
+                    />
+                </GoogleMapReact>
+            </div>
+        );
+    }
+}
+
+export default SimpleMap;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import SimpleMap from './components/simplemap';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+   // <App />, document.getElementById('root')
+    <div style={{width: '400px', height: '400px'}}>
+        <SimpleMap/>
+    </div>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
