164404d58428b1b0d2ea3d7ff62c3de15277abbc
Tom Segbers <tomsegbers@googlemail.com>
2020-01-22 15:55:56 +0100

    Add Material Design Mini Drawer

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import './css/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import SimpleMap from './components/simplemap';
+import Drawer from './components/Drawer';
 
 ReactDOM.render(
-   // <App />, document.getElementById('root')
+    <div style={{width: '100%', height: '100%'}}>
+    <div style={{width: '400px', height: '100%'}}>
+        <Drawer/>
+    </div>,
+    document.getElementById('root')
     <div style={{width: '400px', height: '400px'}}>
         <SimpleMap/>
     </div>,
     document.getElementById('root')
+    </div>,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
