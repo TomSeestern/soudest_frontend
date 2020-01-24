7ee0befbe718dfd0267995eaefe2a23d602906a8
Tom Segbers <tomsegbers@googlemail.com>
2020-01-24 17:51:02 +0100

    Made TransportSelect Buttons togglable!

--- src/components/TransportSelector.js
+++ src/components/TransportSelector.js
@@ -18,76 +18,104 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-export default function IconLabelButtons() {
-    const classes = useStyles();
+class TravelersSelector extends React.Component{
 
-    return (
-        <div className="TransportSelectorRoot">
-            <IconButton
-                variant="contained"
-                color="primary"
-                size="medium"
-                className={classes.button}
-                startIcon={<LocalTaxiIcon />}
-            >
-                <LocalTaxiIcon />
-            </IconButton>
-            <IconButton
-                variant="contained"
-                color="primary"
-                size="medium"
-                className={classes.button}
-                startIcon={<DirectionsBusIcon />}
-            >
-                <DirectionsBusIcon />
-            </IconButton>
-            {/* This Button uses a Font Icon, see the installation instructions in the Icon component docs. */}
-            <IconButton
-                variant="contained"
-                color="primary"
-                size="medium"
-                className={classes.button}
-                startIcon={<TrainIcon/>}
-            >
-                <TrainIcon/>
-            </IconButton>
-            <IconButton
-                variant="contained"
-                color="primary"
-                size="medium"
-                className={classes.button}
-                startIcon={<TramIcon />}
-            >
-                <TramIcon />
-            </IconButton>
-            <IconButton
-                variant="contained"
-                color="primary"
-                size="medium"
-                className={classes.button}
-                startIcon={<SubwayIcon />}
-            >
-                <SubwayIcon />
-            </IconButton>
-            <IconButton
-                variant="contained"
-                color="primary"
-                size="medium"
-                className={classes.button}
-                startIcon={<FlightIcon />}
-            >
-                <FlightIcon />
-            </IconButton>
-            <IconButton
-                variant="contained"
-                color="primary"
-                size="medium"
-                disabled
-                className={classes.button}
-                startIcon={<DirectionsBoatIcon />}
-            >
-                <DirectionsBoatIcon/>
-            </IconButton>
-        </div>
-    );
+    constructor(props) {
+        super(props);
+        //const classes = useStyles();
+        this.state = {taxi:true,bus:true,train:true,tram:true,subway:true,flight:true,boat:true}
+    }
+
+    render(){
+        return (
+            <div className="TransportSelectorRoot">
+                <IconButton
+                    id="taxi"
+                    variant="contained"
+                    color={this.state.taxi ? 'primary' : "grey"}
+                    size="medium"
+                    className="TransportButton"
+                    onClick={event => this.setState({ taxi: !this.state.taxi })}
+                    startIcon={<LocalTaxiIcon />}
+                >
+                    <LocalTaxiIcon />
+                </IconButton>
+                <IconButton
+                    id="bus"
+                    variant="contained"
+                    color={this.state.bus ? 'primary' : "grey"}
+                    onClick={event => this.setState({ bus: !this.state.bus })}
+                    color="primary"
+                    size="medium"
+                    className="TransportButton"
+
+                    startIcon={<DirectionsBusIcon />}
+                >
+                    <DirectionsBusIcon />
+                </IconButton>
+                {/* This Button uses a Font Icon, see the installation instructions in the Icon component docs. */}
+                <IconButton
+                    id="train"
+                    variant="contained"
+                    color="primary"
+                    size="medium"
+                    className="TransportButton"
+                    color={this.state.train ? 'primary' : "grey"}
+                    onClick={event => this.setState({ train: !this.state.train })}
+                    startIcon={<TrainIcon/>}
+                >
+                    <TrainIcon/>
+                </IconButton>
+                <IconButton
+                    id="tram"
+                    variant="contained"
+                    color="primary"
+                    size="medium"
+                    className="TransportButton"
+                    color={this.state.tram ? 'primary' : "grey"}
+                    onClick={event => this.setState({ tram: !this.state.tram })}
+                    startIcon={<TramIcon />}
+                >
+                    <TramIcon />
+                </IconButton>
+                <IconButton
+                    id="subway"
+                    variant="contained"
+                    color="primary"
+                    size="medium"
+                    className="TransportButton"
+                    color={this.state.subway ? 'primary' : "grey"}
+                    onClick={event => this.setState({ subway: !this.state.subway })}
+                    startIcon={<SubwayIcon />}
+                >
+                    <SubwayIcon />
+                </IconButton>
+                <IconButton
+                    id="flight"
+                    variant="contained"
+                    color="primary"
+                    size="medium"
+                    className="TransportButton"
+                    color={this.state.flight ? 'primary' : "grey"}
+                    onClick={event => this.setState({ flight: !this.state.flight })}
+                    startIcon={<FlightIcon />}
+                >
+                    <FlightIcon />
+                </IconButton>
+                <IconButton
+                    id="boat"
+                    variant="contained"
+                    color="primary"
+                    size="medium"
+                    className="TransportButton"
+                    color={this.state.boat ? 'primary' : "grey"}
+                    onClick={event => this.setState({ boat: !this.state.boat })}
+                    startIcon={<DirectionsBoatIcon />}
+                >
+                    <DirectionsBoatIcon/>
+                </IconButton>
+            </div>
+        )
+    }
 }
+export default TravelersSelector;
