fc3e726225af7652d22c35eac9f65f5d96beb689
Tom Segbers <tomsegbers@googlemail.com>
2020-01-24 11:09:46 +0100

    Revert "Add Material Design Mini Drawer"
    This reverts commit 164404d5

--- src/index.js
+++ src/index.js
@@ -4,19 +4,13 @@ import './css/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import SimpleMap from './components/simplemap';
-import Drawer from './components/Drawer';
 
 ReactDOM.render(
-    <div style={{width: '100%', height: '100%'}}>
-    <div style={{width: '400px', height: '100%'}}>
-        <Drawer/>
-    </div>,
-    document.getElementById('root')
+   // <App />, document.getElementById('root')
     <div style={{width: '400px', height: '400px'}}>
         <SimpleMap/>
     </div>,
     document.getElementById('root')
-    </div>,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
