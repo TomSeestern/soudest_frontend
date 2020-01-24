dfa8862fd37657e6b58fda08c0fcd534e771a067
Tom Segbers <tomsegbers@googlemail.com>
2020-01-24 11:34:44 +0100

    Add Grid Layout

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import App from './components/App';
+import RootLayout from './components/RootLayout';
 import * as serviceWorker from './serviceWorker';
-import SimpleMap from './components/simplemap';
 
 ReactDOM.render(
-   // <App />, document.getElementById('root')
-    <div style={{width: '400px', height: '400px'}}>
-        <SimpleMap/>
-    </div>,
-    document.getElementById('root')
+    <RootLayout />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
