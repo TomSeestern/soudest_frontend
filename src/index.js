55d2c22886ad91a4ee828bb4071b0bbf548d2f24
Tom Segbers <tomsegbers@googlemail.com>
2020-02-08 19:24:32 +0100

    Add SignIn/Up FrontendDummy and HashRouter

--- src/index.js
+++ src/index.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import RootLayout from './components/RootLayout';
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-    <RootLayout />, document.getElementById('root')
-);
+import {HashRouter,Route,Link} from 'react-router-dom'
 
+ReactDOM.render(
+    <HashRouter>
+        <div>
+            <Route exact path="/" component={RootLayout}/>
+            <Route exact path="/index" component={RootLayout}/>
+            <Route exact path="/SignIn" component={SignIn}/>
+            <Route exact path="/SignUp" component={SignUp}/>
+        </div>
+    </HashRouter>,document.getElementById('root')
+
+
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
