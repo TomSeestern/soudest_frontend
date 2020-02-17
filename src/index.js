bdbcbc5d9c3b2f504084d7abe31f0129fa24a684
Tom Segbers <tomsegbers@googlemail.com>
2020-02-17 11:07:50 +0100

    Add very simple Profile Page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './css/index.css';
 import RootLayout from './components/RootLayout';
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import ProfilePage from "./components/ProfilePage";
 import * as serviceWorker from './serviceWorker';
 
 import {HashRouter,Route,Link} from 'react-router-dom'
@@ -15,6 +16,7 @@ ReactDOM.render(
             <Route exact path="/index" component={RootLayout}/>
             <Route exact path="/SignIn" component={SignIn}/>
             <Route exact path="/SignUp" component={SignUp}/>
+            <Route exact path="/Profile" component={ProfilePage}/>
         </div>
     </HashRouter>,document.getElementById('root')
 
